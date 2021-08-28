2aef696a7a8ebd65d2a1a5c3ee8c86ac2b3fd71e
AdilsonAngelo <adilson.angelo.jr@gmail.com>
2021-08-28 09:47:48 -0300

    Adds DB context

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Todos.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>()
+                .Property(p => p.Title)
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Todo>()
+                .Property(p => p.Description)
+                .HasMaxLength(500);
+
+            modelBuilder.Entity<Todo>()
+                .HasData(
+                    new Todo { Id = 1, Title = "Shopping list", Description = "tomato, onions, sauce"}
+                );
+        }
+    }
+}
